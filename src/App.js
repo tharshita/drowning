4eacd7734b47243c1b321b5396fd470255f31c7c
Tiyari Harshita <e0316032@u.nus.edu>
2021-01-08 22:14:29 +0800

    enter coordinates to see if land is water

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import human from './human.png';
+import React, { Component } from 'react';
 import './App.css';
+import { Container, Button, Form, Modal} from 'semantic-ui-react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      showModal: false,
+      drowning: false
+    };
+
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
+  }
+
+  onChangeHandler = (event) => {
+    const value = event.target.value;
+    this.setState({
+        [event.target.name] : value
+    })
+  }
+
+  handleSearch = async () => {
+    const search = {
+      longitude: parseFloat(this.state.longitude).toFixed(2),
+      latitude: parseFloat(this.state.latitude).toFixed(2)
+    }
+    console.log(search.latitude)
+    console.log(search.longitude)
+    await axios.get("https://api.onwater.io/api/v1/results/" + search.latitude + "," + search.longitude + "?")
+    .then(response => {
+      this.setState({
+        showModal: true,
+        drowning: response.data.water
+      });
+      console.log(this.state.showModal)
+    })
+    .catch(error => console.error(error))
+  }
+
+  handleModalClose = () => {
+    this.setState({
+        showModal : false
+    })
+  }
+
+  render() {
+    const {showModal} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={human} className="App-logo" alt="logo" />
+          <p>
+            Are you drowning?
+          </p>
+        </header>
+        <Container>
+          <Form>
+            <Form.Group widths='equal'>
+              <Form.Field label='latitude'  name='latitude' min='-85' max='85' step="0.01" type='number' control='input'  required onChange={this.onChangeHandler}/>
+              <Form.Field label='longitude' name='longitude' min='-180' max='180' step="0.01" type='number' control='input' required onChange={this.onChangeHandler}/>
+            </Form.Group>
+            <Button variant="primary" onClick={this.handleSearch}>
+              Find me!
+            </Button>
+          </Form>
+        </Container>
+        <Modal open={showModal} onClose={this.handleModalClose}>
+          <Modal.Header closeButton>
+            Where am I?
+          </Modal.Header>
+          <Modal.Content>
+            {this.drowning? 'You\'re drowning': 'You\'re safe'}
+          </Modal.Content>
+        </Modal>
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
